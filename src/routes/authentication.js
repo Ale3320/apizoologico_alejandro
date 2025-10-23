33fdac915de95ddf82efefd5642c5fc0162de433
Ale3320 <alejandro.parada2307@gmail.com>
2025-10-23 18:55:26 -0500

    se ejecuta el coomando npm i jsonwebtoken --save

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const router = express.Router(); //manejador de rutas de express
 const userSchema = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+// router.post("/signup", async (req, res) => {
+//   const { usuario, correo, clave } = req.body;
+//   const user = new userSchema({
+//     usuario: usuario,
+//     correo: correo,
+//     clave: clave,
+//   });
+//   user.clave = await user.encryptClave(user.clave);
+//   await user.save(); //save es un método de mongoose para guardar datos en MongoDB
+//   //es.json(user);
+//   res.json({
+//     message: "Usuario guardado.",
+//   });
+// });
 
 router.post("/signup", async (req, res) => {
   const { usuario, correo, clave } = req.body;
@@ -10,10 +26,14 @@ router.post("/signup", async (req, res) => {
     clave: clave,
   });
   user.clave = await user.encryptClave(user.clave);
-  await user.save(); //save es un método de mongoose para guardar datos en MongoDB
-  //es.json(user);
+  await user.save(); //save es un método de mongoose para guardar datos en MongoDB //segundo parámetro: un texto que hace que el código generado sea único //tercer parámetro: tiempo de expiración (en segundos, 24 horas en segundos) //primer parámetro: payload - un dato que se agrega para generar el token
+  const token = jwt.sign({ id: user._id }, process.env.SECRET, {
+    expiresIn: 60 * 60 * 24, //un día en segundos
+  });
   res.json({
-    message: "Usuario guardado.",
+    auth: true,
+    token,
   });
 });
+
 module.exports = router;
